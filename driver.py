6fec442270dc17d78326d4f75b3c6cacfa7255c7
bryceschumacher <bryce.schumacher8@gmail.com>
2021-05-28 13:28:07 -0600

    second commit, added driver functionality

--- driver.py
+++ driver.py
@@ -9,8 +9,64 @@ MAX_FPS = 15
 IMAGES = {}
 
 
-def load_images():
+def loadImages():
     pieces = ["wp", "bp", "wR", "bR", "wN", "bN", "wB", "bB", "wK", "bK", "wQ", "bQ"]
     for piece in pieces:
-        IMAGES[piece] = p.image.load("images/" + piece + ".png")
-        
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = chessGame.GameState()
+    print(gs.board)
+    loadImages()
+    running = True
+    sqSelected = ()
+    playerClicks = []
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                # can add functionality of dragging here, currently only works with clicking current and future piece location
+                location = p.mouse.get_pos()
+                col = location[0]//SQ_SIZE
+                row = location[1]//SQ_SIZE
+                if sqSelected == (row,col):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row,col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    
+
+        drawChessGame(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawChessGame(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colors = [p.Color(235, 235, 208), p.Color(119, 148, 85)]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+
+if __name__ == "__main__":
+    main()
